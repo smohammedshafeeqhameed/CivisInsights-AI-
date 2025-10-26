484755a99147e9e60b93b36d329196e365022a07
Projects Training <shafeeq.project.training@gmail.com>
2025-10-26 13:15:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7343744148-98554",
+  "appId": "1:983347948713:web:d10632f0f2b518c6988db8",
+  "apiKey": "AIzaSyDhUkzLEtI5LxHNLYZlv2W3d5JQqw8G0RE",
+  "authDomain": "studio-7343744148-98554.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "983347948713"
+};
